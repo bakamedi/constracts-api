cef211cf11eb46ba6ad2d3004d18c00129b746c8
Bakke Medina <bakamedi12@gmail.com>
2023-12-24 17:33:58 -0500

    NEW interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('v1/api');
+  app.useGlobalInterceptors(new ResponseInterceptor());
+
   await app.listen(3000);
 }
 bootstrap();
